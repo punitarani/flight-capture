379e88375fa59759971a45b8106c7f261e74a60f
Punit Sai Arani <punitsai36@gmail.com>
2021-12-30 00:35:47 -0700

    Fix s.longitutde and s.latitude

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     api = OpenSkyApi(username=opensky_username, password=opensky_password)
     states = api.get_states()
     for s in states.states:
-        print("(%r, %r, %r, %r)" % (s.reference_longitude, s.reference_latitude, s.baro_altitude, s.velocity))
+        print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
