f6ed87691f4c4eab002f2f8e4e41df977c9b0e1d
Punit Sai Arani <punitsai36@gmail.com>
2021-12-29 20:22:15 -0700

    Fix comments

--- opensky.py
+++ opensky.py
@@ -1,23 +1,5 @@
-# Official OpenSky Network API client implementation
-#
-# Author: Markus Fuchs <fuchs@opensky-network.org>
-# URL:    http://github.com/openskynetwork/opensky-api
-#
-# Dependencies: requests (http://docs.python-requests.org/)
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# OpenSky Network API wrapper
+
 import calendar
 import logging
 import pprint
