ab7e9739abb32fadb14b9aab3b9abcc0d127a4f1
Punit Sai Arani <punitsai36@gmail.com>
2021-12-30 00:33:13 -0700

    Update to use OpenSky username and password

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from opensky import OpenSkyApi
+from config.config import opensky_username, opensky_password
 
 if __name__ == '__main__':
-    api = OpenSkyApi()
+    api = OpenSkyApi(username=opensky_username, password=opensky_password)
     states = api.get_states()
     for s in states.states:
-        print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
+        print("(%r, %r, %r, %r)" % (s.reference_longitude, s.reference_latitude, s.baro_altitude, s.velocity))
