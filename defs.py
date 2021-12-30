36920b358604d1a5a6f00bbc61246e7611806cc7
Punit Sai Arani <punitsai36@gmail.com>
2021-12-30 16:04:06 -0700

    Initial commit

---
+++ defs.py
@@ -0,0 +1,20 @@
+# Definitions File
+
+import os
+import sys
+
+# OS
+PLATFORM = sys.platform
+
+# Directories
+ROOT = os.path.dirname(os.path.abspath(__file__))
+TEMP = os.path.join(ROOT, "temp")
+LOGS = os.path.join(ROOT, "logs")
+
+# Log Files
+SYSTEM_LOG = os.path.join(LOGS, "system.log")
+
+# Config Files
+CONFIG_DIR = os.path.join(ROOT, "config")
+CONFIG_JSON = os.path.join(CONFIG_DIR, "config.json")
+CONFIG_TEMPLATE_JSON = os.path.join(CONFIG_DIR, "config_template.json")
